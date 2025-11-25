6bc622debd0b9649cb6d50d7fda0cf4f121eef0e
Leandre <leandre@example.com>
2025-11-25 12:13:01 +0200

    chore: add utilities functionality (iteration 43)

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ Main Flask application for Barcode Decoder
     # Commit 35: 2025-11-25 12:12:58
 """
     # Commit 29: 2025-11-25 12:12:56
+    # Commit 43: 2025-11-25 12:13:01
 from flask import Flask, render_template, request, jsonify
 import os
 from werkzeug.utils import secure_filename
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 """
 Application entry point
+    # Commit 43: 2025-11-25 12:13:01
     # Commit 18: 2025-11-25 12:12:53
     # Commit 12: 2025-11-25 12:12:51
 """
