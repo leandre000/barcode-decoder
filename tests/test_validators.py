b3c61fd83d0c87e8b2068621da665d3c3bd6b4be
Leandre <leandre@example.com>
2025-11-25 12:13:20 +0200

    chore: fix decoder functionality (iteration 97)

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -7,6 +7,7 @@ Tests for validators
 import unittest
 from utils.validators import validate_image_file, validate_file_size
 from werkzeug.datastructures import FileStorage
+    # Commit 97: 2025-11-25 12:13:20
     # Commit 59: 2025-11-25 12:13:06
 from io import BytesIO
 
