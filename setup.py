ec1e1779687e48e088bd6f0d5710471f1f947326
Leandre <leandre@example.com>
2025-11-25 13:10:25 +0200

    build: add setup.py for package distribution

--- setup.py
+++ setup.py
@@ -1,32 +1,13 @@
 """
-    # Commit 158: 2025-11-25 12:13:41
-    # Commit 140: 2025-11-25 12:13:34
-    # Commit 129: 2025-11-25 12:13:30
-    # Commit 189: 2025-11-25 12:13:52
-    # Commit 152: 2025-11-25 12:13:39
-    # Commit 194: 2025-11-25 12:13:54
+
 Setup script for Barcode Decoder
-    # Commit 94: 2025-11-25 12:13:19
-    # Commit 168: 2025-11-25 12:13:45
-    # Commit 42: 2025-11-25 12:13:00
-    # Commit 128: 2025-11-25 12:13:30
-    # Commit 67: 2025-11-25 12:13:09
-    # Commit 1: 2025-11-25 12:12:48
+
 """
 from setuptools import setup, find_packages
-    # Commit 123: 2025-11-25 12:13:28
-    # Commit 64: 2025-11-25 12:13:08
-    # Commit 6: 2025-11-25 12:12:49
-    # Commit 109: 2025-11-25 12:13:24
 
-    # Commit 85: 2025-11-25 12:13:16
-    # Commit 77: 2025-11-25 12:13:13
 with open("README.md", "r", encoding="utf-8") as fh:
-    # Commit 65: 2025-11-25 12:13:08
-    # Commit 56: 2025-11-25 12:13:05
-    # Commit 62: 2025-11-25 12:13:07
+
     long_description = fh.read()
-    # Commit 52: 2025-11-25 12:13:04
 
 with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
